df51ee40fb40b6844803b92c6ceae4e4a7b20c69
P H <git@cubt85iz>
2024-12-25 10:36:00 -0500

    Adds ability to exclude folders

--- missing_trailers.py
+++ missing_trailers.py
@@ -10,6 +10,7 @@ from jellyfin_api_client.models import ItemFields, BaseItemKind, BaseItemDto
 
 JELLYFIN_ENDPOINT = os.environ("JELLYFIN_ENDPOINT")
 JELLYFIN_TOKEN = os.environ("JELLYFIN_TOKEN")
+JLT_EXCLUDED_PATHS = os.environ("JLT_EXCLUDED_PATHS").split(",")
 
 # Load collections from configuration file
 if os.path.exists('config.json'):
@@ -23,6 +24,7 @@ jellyfin_client = AuthenticatedClient(
   prefix="MediaBrowser",
 )
 
+# Identify the Jellyfin movies that have trailers.
 jellyfin_movies_with_trailers = get_items.sync(
   client=jellyfin_client,
   include_item_types=[BaseItemKind.MOVIE],
@@ -31,16 +33,24 @@ jellyfin_movies_with_trailers = get_items.sync(
   recursive=True
 ).items
 
+# Identify the Jellyfin movies that aren't excluded that are missing local trailers.
 jellyfin_movies_without_local_trailers=[]
 for movie in jellyfin_movies_with_trailers:
-  if movie.local_trailer_count == 0:
+  excluded = False
+  for folder in JLT_EXCLUDED_PATHS:
+    if movie.path.startswith(folder):
+      excluded = True
+      break
+
+  if movie.local_trailer_count == 0 and not excluded:
     jellyfin_movies_without_local_trailers.append(movie)
 
+# Download missing local trailers.
 for movie in jellyfin_movies_without_local_trailers:
   # Mounts must mirror those in Jellyfin for this to work.
-  #local_trailer_path=os.path.dirname(movie.path) + "/" + os.path.splitext(os.path.basename(movie.path))[0] + "-trailer.mp4"
-  local_trailer_path=os.path.splitext(os.path.basename(movie.path))[0] + "-trailer.mp4"
+  local_trailer_path=os.path.dirname(movie.path) + "/" + os.path.splitext(os.path.basename(movie.path))[0] + "-trailer.mp4"
   trailer_url=movie.remote_trailers[0].url
+  
   ydl_opts = {
     "quiet": True,
     "outtmpl": f"{local_trailer_path}",
