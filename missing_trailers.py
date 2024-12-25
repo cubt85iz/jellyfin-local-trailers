05647d5698934c7bc0b9d35a42b8a30ac7054773
P H <git@cubt85iz>
2024-12-25 09:09:13 -0500

    Initial commit

---
+++ missing_trailers.py
@@ -0,0 +1,50 @@
+import json
+import os
+import yt_dlp
+
+from jellyfin_api_client import AuthenticatedClient
+from jellyfin_api_client.api.collection import add_to_collection, create_collection, remove_from_collection
+from jellyfin_api_client.api.items import get_items
+from jellyfin_api_client.api.item_update import update_item
+from jellyfin_api_client.models import ItemFields, BaseItemKind, BaseItemDto
+
+JELLYFIN_ENDPOINT = os.environ("JELLYFIN_ENDPOINT")
+JELLYFIN_TOKEN = os.environ("JELLYFIN_TOKEN")
+
+# Load collections from configuration file
+if os.path.exists('config.json'):
+  with open('config.json', 'r') as config_json:
+    config = json.load(config_json)
+
+# Create client for Jellyfin
+jellyfin_client = AuthenticatedClient(
+  base_url=JELLYFIN_ENDPOINT,
+  token=f'Token={JELLYFIN_TOKEN}',
+  prefix="MediaBrowser",
+)
+
+jellyfin_movies_with_trailers = get_items.sync(
+  client=jellyfin_client,
+  include_item_types=[BaseItemKind.MOVIE],
+  fields=[ItemFields.LOCALTRAILERCOUNT, ItemFields.PATH, ItemFields.REMOTETRAILERS],
+  has_trailer=True,
+  recursive=True
+).items
+
+jellyfin_movies_without_local_trailers=[]
+for movie in jellyfin_movies_with_trailers:
+  if movie.local_trailer_count == 0:
+    jellyfin_movies_without_local_trailers.append(movie)
+
+for movie in jellyfin_movies_without_local_trailers:
+  # Mounts must mirror those in Jellyfin for this to work.
+  #local_trailer_path=os.path.dirname(movie.path) + "/" + os.path.splitext(os.path.basename(movie.path))[0] + "-trailer.mp4"
+  local_trailer_path=os.path.splitext(os.path.basename(movie.path))[0] + "-trailer.mp4"
+  trailer_url=movie.remote_trailers[0].url
+  ydl_opts = {
+    "quiet": True,
+    "outtmpl": f"{local_trailer_path}",
+    "format": "bestvideo[ext=mp4]+bestaudio[ext=m4a]/mp4"
+  }
+  with yt_dlp.YoutubeDL(ydl_opts) as ydl:
+    ydl.download([trailer_url])
